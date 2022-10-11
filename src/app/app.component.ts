b00e3f6327e206b8694ebfa5ce266dcad9554a68
DA M1 33 <dam133@DAs-MacBook-Air-33.local>
2022-10-11 16:11:21 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'todolist';
+export class AppComponent implements OnInit {
+  saveData: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.saveData = this.formBuilder.group({
+      name: '',
+    });
+  }
+
+  onSubmit(form: FormGroup) {
+    console.log('Name', form.value.name);
+  }
 }
